b1b8ee3081fffb57992878af3b219ac6d29914a8
zcareyltu <zcarey@ltu.edu>
2019-11-14 00:31:43 -0500

    Sends and receives data

--- Ethernet Performance - Computer/MainForm.cs
+++ Ethernet Performance - Computer/MainForm.cs
@@ -13,7 +13,8 @@ using System.Windows.Forms;
 namespace Ethernet_Performance___Computer {
 	public partial class MainForm : Form {
 
-		private const int PORT = 6003;
+		private const int DEST_PORT = 6001;
+		private const int RECV_PORT = 6002;
 		private const string IP = "169.254.240.157"; //"255.255.255.255";
 
 		private UdpClient client;
@@ -27,7 +28,14 @@ namespace Ethernet_Performance___Computer {
 			ConnectBtn.Enabled = false;
 
 			client = new UdpClient();
-			client.Client.Bind(new IPEndPoint(IPAddress.Any, PORT));
+			client.Client.Bind(new IPEndPoint(IPAddress.Any, RECV_PORT));
+
+			try {
+				client.BeginReceive(new AsyncCallback(OnDataReceived), null);
+			}catch(Exception ex) {
+				MessageBox.Show(ex.ToString());
+			}
+			//client.Client.Bind(new IPEndPoint(IPAddress.Any, DEST_PORT));
 		}
 
 		private void DisconnectBtn_Click(object sender, EventArgs e) {
@@ -41,8 +49,17 @@ namespace Ethernet_Performance___Computer {
 		private void SendBtn_Click(object sender, EventArgs e) {
 			if(client != null) {
 				byte[] data = Encoding.UTF8.GetBytes(MsgTextBox.Text);
-				client.Send(data, data.Length, IP, PORT);
+				client.Send(data, data.Length, IP, DEST_PORT);
 			}
+		}
+
+
+		private void OnDataReceived(IAsyncResult res) {
+			IPEndPoint ip = new IPEndPoint(IPAddress.Any, RECV_PORT);
+			byte[] data = client.EndReceive(res, ref ip);
+
+			Console.WriteLine(Encoding.UTF8.GetString(data));
+			client.BeginReceive(new AsyncCallback(OnDataReceived), null);
 		}
 	}
 }
